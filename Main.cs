48670767a64e64a914e061f99abf24b1c13a4868
YeahSeptember <yeahsept@yeah.net>
2025-12-13 19:50:56 +0800

    添加项目文件。

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using NaHCO3;
+
+namespace NaHCO3Roles
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(NaHCO3.Main.PLUGIN_GUID)]
+    public class Main : BasePlugin
+    {
+        public override void Load()
+        {
+            Logger.Info("碳酸氢钠的职业附加加载成功!", "NaHCO3Roles");
+        }
+    }
+}
